ecf51f0cd4dee206008a3227f703e63b0b993856
Austin <Harrenaustint@gmail.com>
2021-12-06 14:40:21 -0500

    pushing working database

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import { Route } from 'react-router'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About'
+import {BrowserRouter} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <div>
+        <BrowserRouter>
+
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
+        </BrowserRouter>
       </div>
     )
   }
